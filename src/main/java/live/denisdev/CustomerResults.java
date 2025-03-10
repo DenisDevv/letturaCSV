a808c426994c98a0e65fd5206673e25e63a4ebcb
DenisDevv <karajedenis@gmail.com>
2025-03-10 12:23:59 +0100

    load customer records from CSV file in constructor

--- src/main/java/live/denisdev/CustomerResults.java
+++ src/main/java/live/denisdev/CustomerResults.java
@@ -1,10 +1,25 @@
 package live.denisdev;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class CustomerResults {
     private ArrayList<CustomerRecord> records = new ArrayList<>();
 
+    public CustomerResults() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/customers-100.csv"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] campi = line.split(",");
+                CustomerRecord record = new CustomerRecord(campi[0], campi[1], campi[2], campi[3], campi[4], campi[5], campi[6], campi[7], campi[8], campi[9], campi[10], campi[11]);
+                addRecord(record);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public void addRecord(CustomerRecord record) {
         records.add(record);
     }
--- src/main/java/live/denisdev/Main.java
+++ src/main/java/live/denisdev/Main.java
@@ -8,16 +8,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         CustomerResults risultati = new CustomerResults();
-        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/customers-100.csv"))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] campi = line.split(",");
-                CustomerRecord record = new CustomerRecord(campi[0], campi[1], campi[2], campi[3], campi[4], campi[5], campi[6], campi[7], campi[8], campi[9], campi[10], campi[11]);
-                risultati.addRecord(record);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         Scanner in = new Scanner(System.in);
         System.out.println("Inserisci il paese di cui vuoi visualizzare i clienti:");
         String paese = in.nextLine();
